ba57901d1524fe5a90d777a1e2b350038633fe6d
Stalloyde <stalloyde93@gmail.com>
2024-03-08 12:24:39 +0800

    Set up cors and multer

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+require('./db.js');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer();
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', upload.none(), indexRouter);
 
 module.exports = app;
