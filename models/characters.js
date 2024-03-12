106fbf5fa7dc10ef1a61c79d34f012860215eeea
Stalloyde <stalloyde93@gmail.com>
2024-03-12 14:13:48 +0800

    Move characterFound from db to state

--- models/characters.js
+++ models/characters.js
@@ -5,7 +5,6 @@ const characterSchema = new Schema({
   name: { type: String, required: true },
   xCoordinate: { type: Number, required: true },
   yCoordinate: { type: Number, required: true },
-  found: { type: Boolean },
 });
 
 module.exports = mongoose.model('Characters', characterSchema);
