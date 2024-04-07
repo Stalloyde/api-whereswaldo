fe97a40b4dfaa408ad1fd72f8cc70d1cb7cb4bb4
Stalloyde <stalloyde93@gmail.com>
2024-04-07 14:20:29 +0800

    Implement score submission

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,31 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Characters = require('../models/characters');
+const Score = require('../models/score');
 const expressAsyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
 let startTime = 0;
 let elapsedTime = 0;
 let id;
+let gameScore = 0;
 
 function setTimer(status) {
   if (status) id = setInterval(updateTime, 10);
-  if (!status) clearInterval(id);
+  if (!status) {
+    gameScore = elapsedTime;
+  }
+}
+
+function resetTimer() {
+  clearInterval(id);
 }
 
 function updateTime() {
   elapsedTime = new Date().getTime() - startTime;
 }
 
-router.get('/', (req, res) => {
-  startTime = new Date().getTime();
-  setTimer(true);
-  return res.json('Game Starts');
-});
-
-router.get('/gameover', (req, res) => {
-  setTimer(false);
+function getFormattedTime(gameScore = elapsedTime) {
   const minCalc = Math.floor((elapsedTime / 1000 / 60) << 0);
   const secCalc = Math.floor((elapsedTime / 1000) % 60);
   const msCalc = ((elapsedTime / 1000) % 60).toFixed(2).slice(-2);
@@ -34,8 +35,38 @@ router.get('/gameover', (req, res) => {
   const seconds = secCalc.toString().padStart(2, '0');
   const milliseconds = msCalc.toString().padStart(2, '0');
   const formattedTime = `${minutes}:${seconds}:${milliseconds}`;
-  return res.json(formattedTime);
+  return formattedTime;
+}
+
+router.get('/', (req, res) => {
+  resetTimer();
+  startTime = new Date().getTime();
+  setTimer(true);
+  return res.json('Game Starts');
 });
+
+router.get('/gameover', (req, res) => {
+  setTimer(false);
+  return res.json(getFormattedTime());
+});
+
+router.post('/gameover', [
+  body('username').notEmpty().withMessage('input is empty').trim().escape(),
+
+  expressAsyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.json(errors);
+
+    const newScore = new Score({
+      score: gameScore,
+      username: req.body.username,
+    });
+
+    await newScore.save();
+
+    return res.json('Score saved');
+  }),
+]);
 
 router.post('/', [
   body('targetCharacterName')
